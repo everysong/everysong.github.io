aea031c352e531f2d601a9e2e4e45a618972413a
everysong <jaymesborth@icloud.com>
2025-09-24 07:56:21 -0400

    Update songs_data.js

--- js/songs_data.js
+++ js/songs_data.js
@@ -4,8 +4,9 @@ const songs = [
     title: "First Song",
     artistId: "ART001",
     cover: "/cdn/songs/first.png",
-    created: 1714000000,
-    baseStreams: 5000
+    created: 1714000000, // upload UNIX
+    baseStreams: 5000,
+    streamMultiplier: 0.02 // streams per second
   },
   {
     id: "SNG002",
@@ -13,7 +14,8 @@ const songs = [
     artistId: "ART002",
     cover: "/cdn/songs/second.png",
     created: 1714050000,
-    baseStreams: 3000
+    baseStreams: 3000,
+    streamMultiplier: 0.05
   },
   {
     id: "SNG003",
@@ -21,6 +23,7 @@ const songs = [
     artistId: "ART003",
     cover: "/cdn/songs/third.png",
     created: 1714100000,
-    baseStreams: 8000
+    baseStreams: 8000,
+    streamMultiplier: 0.01
   }
 ];
