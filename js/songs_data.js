f6aff427a31de4340fb02417bca159506be9229b
everysong <jaymesborth@icloud.com>
2025-09-24 12:26:20 -0400

    Update songs_data.js

--- js/songs_data.js
+++ js/songs_data.js
@@ -3,7 +3,7 @@ const songs = [
     id: "SNG001",
     title: "First Song",
     artistId: "ART001",
-    cover: "/cdn/songs/first.png",
+    cover: "/cdn/placeholder.png",
     created: 1714000000, // upload UNIX
     baseStreams: 5000,
     streamMultiplier: 7 // streams per second
@@ -12,7 +12,7 @@ const songs = [
     id: "SNG002",
     title: "Second Track",
     artistId: "ART002",
-    cover: "/cdn/songs/second.png",
+    cover: "/cdn/placeholder.png",
     created: 1714050000,
     baseStreams: 3000,
     streamMultiplier: 0.05
@@ -21,7 +21,7 @@ const songs = [
     id: "SNG003",
     title: "Third Hit",
     artistId: "ART003",
-    cover: "/cdn/songs/third.png",
+    cover: "/cdn/placeholder.png",
     created: 1714100000,
     baseStreams: 8000,
     streamMultiplier: 0.01
