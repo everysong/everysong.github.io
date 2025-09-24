d0d69896fc14ea06de0a5517462fc58376b6daf8
everysong <jaymesborth@icloud.com>
2025-09-24 03:26:32 -0400

    Update artists_data.js

--- js/artists_data.js
+++ js/artists_data.js
@@ -1,14 +1,20 @@
-// window.ARTISTS -- simple JS data file. Each item on one line.
-// Fields: name, id, pfp (image link), creationDate (unix timestamp)
-window.ARTISTS = [
-  { name: "Lunar Kids", id: "ART001", pfp: "/cdn/icon.png", creationDate: 1672531200 },
-  { name: "Rose Harbor", id: "ART002", pfp: "/cdn/icon.png", creationDate: 1680000000 },
-  { name: "Paper Boats", id: "ART003", pfp: "/cdn/icon.png", creationDate: 1660000000 },
-  { name: "Tall Pines", id: "ART004", pfp: "/cdn/icon.png", creationDate: 1675000000 },
-  { name: "Night Recipe", id: "ART005", pfp: "/cdn/icon.png", creationDate: 1685000000 },
-  { name: "Fleeting", id: "ART006", pfp: "/cdn/icon.png", creationDate: 1690000000 },
-  { name: "Sir Hollow", id: "ART007", pfp: "/cdn/icon.png", creationDate: 1655000000 },
-  { name: "Echo & The Rust", id: "ART008", pfp: "/cdn/icon.png", creationDate: 1649000000 },
-  { name: "Sample Band 9", id: "ART009", pfp: "/cdn/icon.png", creationDate: 1640000000 },
-  { name: "Sample Band 10", id: "ART010", pfp: "/cdn/icon.png", creationDate: 1630000000 }
+const artists = [
+  {
+    id: "ART001",
+    name: "Sample Artist",
+    image: "/cdn/artists/sample.png",
+    created: 1714000000
+  },
+  {
+    id: "ART002",
+    name: "Another Artist",
+    image: "/cdn/artists/another.png",
+    created: 1714100000
+  },
+  {
+    id: "ART003",
+    name: "Indie Star",
+    image: "/cdn/artists/indie.png",
+    created: 1714200000
+  }
 ];
