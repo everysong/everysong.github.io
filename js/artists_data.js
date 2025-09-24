f8e838d6d8a1857b4e7325c53272de2de30969ae
everysong <jaymesborth@icloud.com>
2025-09-24 12:35:30 -0400

    Update artists_data.js

--- js/artists_data.js
+++ js/artists_data.js
@@ -2,19 +2,19 @@ const artists = [
   {
     id: "100001",
     name: "Sample Artist",
-    image: "/cdn/placeholder.png",
+    image: "placeholder.png",
     created: 1714000000
   },
   {
     id: "100002",
     name: "Another Artist",
-    image: "/cdn/placeholder.png",
+    image: "placeholder.png",
     created: 1714100000
   },
   {
     id: "100003",
     name: "Indie Star",
-    image: "/cdn/placeholder.png",
+    image: "placeholder.png",
     created: 1714200000
   }
 ];
