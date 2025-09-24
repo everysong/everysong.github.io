d8bd211ec94e022ab5b63dad579ff270ae8cc4ed
everysong <jaymesborth@icloud.com>
2025-09-24 15:17:54 -0400

    Update artists_data.js

--- js/artists_data.js
+++ js/artists_data.js
@@ -1,20 +1,902 @@
 const artists = [
-  {
-    id: "100001",
-    name: "Sample Artist",
-    image: "placeholder.png",
-    created: 1714000000
-  },
-  {
-    id: "100002",
-    name: "Another Artist",
-    image: "placeholder.png",
-    created: 1714100000
-  },
-  {
-    id: "100003",
-    name: "Indie Star",
-    image: "placeholder.png",
-    created: 1714200000
-  }
+{
+  "id": "100001",
+  "name": "Artist 1",
+  "image": "placeholder.png",
+  "created": 1758740341
+},
+{
+  "id": "100002",
+  "name": "Artist 2",
+  "image": "placeholder.png",
+  "created": 1758730341
+},
+{
+  "id": "100003",
+  "name": "Artist 3",
+  "image": "placeholder.png",
+  "created": 1758720341
+},
+{
+  "id": "100004",
+  "name": "Artist 4",
+  "image": "placeholder.png",
+  "created": 1758710341
+},
+{
+  "id": "100005",
+  "name": "Artist 5",
+  "image": "placeholder.png",
+  "created": 1758700341
+},
+{
+  "id": "100006",
+  "name": "Artist 6",
+  "image": "placeholder.png",
+  "created": 1758690341
+},
+{
+  "id": "100007",
+  "name": "Artist 7",
+  "image": "placeholder.png",
+  "created": 1758680341
+},
+{
+  "id": "100008",
+  "name": "Artist 8",
+  "image": "placeholder.png",
+  "created": 1758670341
+},
+{
+  "id": "100009",
+  "name": "Artist 9",
+  "image": "placeholder.png",
+  "created": 1758660341
+},
+{
+  "id": "100010",
+  "name": "Artist 10",
+  "image": "placeholder.png",
+  "created": 1758650341
+},
+{
+  "id": "100011",
+  "name": "Artist 11",
+  "image": "placeholder.png",
+  "created": 1758640341
+},
+{
+  "id": "100012",
+  "name": "Artist 12",
+  "image": "placeholder.png",
+  "created": 1758630341
+},
+{
+  "id": "100013",
+  "name": "Artist 13",
+  "image": "placeholder.png",
+  "created": 1758620341
+},
+{
+  "id": "100014",
+  "name": "Artist 14",
+  "image": "placeholder.png",
+  "created": 1758610341
+},
+{
+  "id": "100015",
+  "name": "Artist 15",
+  "image": "placeholder.png",
+  "created": 1758600341
+},
+{
+  "id": "100016",
+  "name": "Artist 16",
+  "image": "placeholder.png",
+  "created": 1758590341
+},
+{
+  "id": "100017",
+  "name": "Artist 17",
+  "image": "placeholder.png",
+  "created": 1758580341
+},
+{
+  "id": "100018",
+  "name": "Artist 18",
+  "image": "placeholder.png",
+  "created": 1758570341
+},
+{
+  "id": "100019",
+  "name": "Artist 19",
+  "image": "placeholder.png",
+  "created": 1758560341
+},
+{
+  "id": "100020",
+  "name": "Artist 20",
+  "image": "placeholder.png",
+  "created": 1758550341
+},
+{
+  "id": "100021",
+  "name": "Artist 21",
+  "image": "placeholder.png",
+  "created": 1758540341
+},
+{
+  "id": "100022",
+  "name": "Artist 22",
+  "image": "placeholder.png",
+  "created": 1758530341
+},
+{
+  "id": "100023",
+  "name": "Artist 23",
+  "image": "placeholder.png",
+  "created": 1758520341
+},
+{
+  "id": "100024",
+  "name": "Artist 24",
+  "image": "placeholder.png",
+  "created": 1758510341
+},
+{
+  "id": "100025",
+  "name": "Artist 25",
+  "image": "placeholder.png",
+  "created": 1758500341
+},
+{
+  "id": "100026",
+  "name": "Artist 26",
+  "image": "placeholder.png",
+  "created": 1758490341
+},
+{
+  "id": "100027",
+  "name": "Artist 27",
+  "image": "placeholder.png",
+  "created": 1758480341
+},
+{
+  "id": "100028",
+  "name": "Artist 28",
+  "image": "placeholder.png",
+  "created": 1758470341
+},
+{
+  "id": "100029",
+  "name": "Artist 29",
+  "image": "placeholder.png",
+  "created": 1758460341
+},
+{
+  "id": "100030",
+  "name": "Artist 30",
+  "image": "placeholder.png",
+  "created": 1758450341
+},
+{
+  "id": "100031",
+  "name": "Artist 31",
+  "image": "placeholder.png",
+  "created": 1758440341
+},
+{
+  "id": "100032",
+  "name": "Artist 32",
+  "image": "placeholder.png",
+  "created": 1758430341
+},
+{
+  "id": "100033",
+  "name": "Artist 33",
+  "image": "placeholder.png",
+  "created": 1758420341
+},
+{
+  "id": "100034",
+  "name": "Artist 34",
+  "image": "placeholder.png",
+  "created": 1758410341
+},
+{
+  "id": "100035",
+  "name": "Artist 35",
+  "image": "placeholder.png",
+  "created": 1758400341
+},
+{
+  "id": "100036",
+  "name": "Artist 36",
+  "image": "placeholder.png",
+  "created": 1758390341
+},
+{
+  "id": "100037",
+  "name": "Artist 37",
+  "image": "placeholder.png",
+  "created": 1758380341
+},
+{
+  "id": "100038",
+  "name": "Artist 38",
+  "image": "placeholder.png",
+  "created": 1758370341
+},
+{
+  "id": "100039",
+  "name": "Artist 39",
+  "image": "placeholder.png",
+  "created": 1758360341
+},
+{
+  "id": "100040",
+  "name": "Artist 40",
+  "image": "placeholder.png",
+  "created": 1758350341
+},
+{
+  "id": "100041",
+  "name": "Artist 41",
+  "image": "placeholder.png",
+  "created": 1758340341
+},
+{
+  "id": "100042",
+  "name": "Artist 42",
+  "image": "placeholder.png",
+  "created": 1758330341
+},
+{
+  "id": "100043",
+  "name": "Artist 43",
+  "image": "placeholder.png",
+  "created": 1758320341
+},
+{
+  "id": "100044",
+  "name": "Artist 44",
+  "image": "placeholder.png",
+  "created": 1758310341
+},
+{
+  "id": "100045",
+  "name": "Artist 45",
+  "image": "placeholder.png",
+  "created": 1758300341
+},
+{
+  "id": "100046",
+  "name": "Artist 46",
+  "image": "placeholder.png",
+  "created": 1758290341
+},
+{
+  "id": "100047",
+  "name": "Artist 47",
+  "image": "placeholder.png",
+  "created": 1758280341
+},
+{
+  "id": "100048",
+  "name": "Artist 48",
+  "image": "placeholder.png",
+  "created": 1758270341
+},
+{
+  "id": "100049",
+  "name": "Artist 49",
+  "image": "placeholder.png",
+  "created": 1758260341
+},
+{
+  "id": "100050",
+  "name": "Artist 50",
+  "image": "placeholder.png",
+  "created": 1758250341
+},
+{
+  "id": "100051",
+  "name": "Artist 51",
+  "image": "placeholder.png",
+  "created": 1758240341
+},
+{
+  "id": "100052",
+  "name": "Artist 52",
+  "image": "placeholder.png",
+  "created": 1758230341
+},
+{
+  "id": "100053",
+  "name": "Artist 53",
+  "image": "placeholder.png",
+  "created": 1758220341
+},
+{
+  "id": "100054",
+  "name": "Artist 54",
+  "image": "placeholder.png",
+  "created": 1758210341
+},
+{
+  "id": "100055",
+  "name": "Artist 55",
+  "image": "placeholder.png",
+  "created": 1758200341
+},
+{
+  "id": "100056",
+  "name": "Artist 56",
+  "image": "placeholder.png",
+  "created": 1758190341
+},
+{
+  "id": "100057",
+  "name": "Artist 57",
+  "image": "placeholder.png",
+  "created": 1758180341
+},
+{
+  "id": "100058",
+  "name": "Artist 58",
+  "image": "placeholder.png",
+  "created": 1758170341
+},
+{
+  "id": "100059",
+  "name": "Artist 59",
+  "image": "placeholder.png",
+  "created": 1758160341
+},
+{
+  "id": "100060",
+  "name": "Artist 60",
+  "image": "placeholder.png",
+  "created": 1758150341
+},
+{
+  "id": "100061",
+  "name": "Artist 61",
+  "image": "placeholder.png",
+  "created": 1758140341
+},
+{
+  "id": "100062",
+  "name": "Artist 62",
+  "image": "placeholder.png",
+  "created": 1758130341
+},
+{
+  "id": "100063",
+  "name": "Artist 63",
+  "image": "placeholder.png",
+  "created": 1758120341
+},
+{
+  "id": "100064",
+  "name": "Artist 64",
+  "image": "placeholder.png",
+  "created": 1758110341
+},
+{
+  "id": "100065",
+  "name": "Artist 65",
+  "image": "placeholder.png",
+  "created": 1758100341
+},
+{
+  "id": "100066",
+  "name": "Artist 66",
+  "image": "placeholder.png",
+  "created": 1758090341
+},
+{
+  "id": "100067",
+  "name": "Artist 67",
+  "image": "placeholder.png",
+  "created": 1758080341
+},
+{
+  "id": "100068",
+  "name": "Artist 68",
+  "image": "placeholder.png",
+  "created": 1758070341
+},
+{
+  "id": "100069",
+  "name": "Artist 69",
+  "image": "placeholder.png",
+  "created": 1758060341
+},
+{
+  "id": "100070",
+  "name": "Artist 70",
+  "image": "placeholder.png",
+  "created": 1758050341
+},
+{
+  "id": "100071",
+  "name": "Artist 71",
+  "image": "placeholder.png",
+  "created": 1758040341
+},
+{
+  "id": "100072",
+  "name": "Artist 72",
+  "image": "placeholder.png",
+  "created": 1758030341
+},
+{
+  "id": "100073",
+  "name": "Artist 73",
+  "image": "placeholder.png",
+  "created": 1758020341
+},
+{
+  "id": "100074",
+  "name": "Artist 74",
+  "image": "placeholder.png",
+  "created": 1758010341
+},
+{
+  "id": "100075",
+  "name": "Artist 75",
+  "image": "placeholder.png",
+  "created": 1758000341
+},
+{
+  "id": "100076",
+  "name": "Artist 76",
+  "image": "placeholder.png",
+  "created": 1757990341
+},
+{
+  "id": "100077",
+  "name": "Artist 77",
+  "image": "placeholder.png",
+  "created": 1757980341
+},
+{
+  "id": "100078",
+  "name": "Artist 78",
+  "image": "placeholder.png",
+  "created": 1757970341
+},
+{
+  "id": "100079",
+  "name": "Artist 79",
+  "image": "placeholder.png",
+  "created": 1757960341
+},
+{
+  "id": "100080",
+  "name": "Artist 80",
+  "image": "placeholder.png",
+  "created": 1757950341
+},
+{
+  "id": "100081",
+  "name": "Artist 81",
+  "image": "placeholder.png",
+  "created": 1757940341
+},
+{
+  "id": "100082",
+  "name": "Artist 82",
+  "image": "placeholder.png",
+  "created": 1757930341
+},
+{
+  "id": "100083",
+  "name": "Artist 83",
+  "image": "placeholder.png",
+  "created": 1757920341
+},
+{
+  "id": "100084",
+  "name": "Artist 84",
+  "image": "placeholder.png",
+  "created": 1757910341
+},
+{
+  "id": "100085",
+  "name": "Artist 85",
+  "image": "placeholder.png",
+  "created": 1757900341
+},
+{
+  "id": "100086",
+  "name": "Artist 86",
+  "image": "placeholder.png",
+  "created": 1757890341
+},
+{
+  "id": "100087",
+  "name": "Artist 87",
+  "image": "placeholder.png",
+  "created": 1757880341
+},
+{
+  "id": "100088",
+  "name": "Artist 88",
+  "image": "placeholder.png",
+  "created": 1757870341
+},
+{
+  "id": "100089",
+  "name": "Artist 89",
+  "image": "placeholder.png",
+  "created": 1757860341
+},
+{
+  "id": "100090",
+  "name": "Artist 90",
+  "image": "placeholder.png",
+  "created": 1757850341
+},
+{
+  "id": "100091",
+  "name": "Artist 91",
+  "image": "placeholder.png",
+  "created": 1757840341
+},
+{
+  "id": "100092",
+  "name": "Artist 92",
+  "image": "placeholder.png",
+  "created": 1757830341
+},
+{
+  "id": "100093",
+  "name": "Artist 93",
+  "image": "placeholder.png",
+  "created": 1757820341
+},
+{
+  "id": "100094",
+  "name": "Artist 94",
+  "image": "placeholder.png",
+  "created": 1757810341
+},
+{
+  "id": "100095",
+  "name": "Artist 95",
+  "image": "placeholder.png",
+  "created": 1757800341
+},
+{
+  "id": "100096",
+  "name": "Artist 96",
+  "image": "placeholder.png",
+  "created": 1757790341
+},
+{
+  "id": "100097",
+  "name": "Artist 97",
+  "image": "placeholder.png",
+  "created": 1757780341
+},
+{
+  "id": "100098",
+  "name": "Artist 98",
+  "image": "placeholder.png",
+  "created": 1757770341
+},
+{
+  "id": "100099",
+  "name": "Artist 99",
+  "image": "placeholder.png",
+  "created": 1757760341
+},
+{
+  "id": "100100",
+  "name": "Artist 100",
+  "image": "placeholder.png",
+  "created": 1757750341
+},
+{
+  "id": "100101",
+  "name": "Artist 101",
+  "image": "placeholder.png",
+  "created": 1757740341
+},
+{
+  "id": "100102",
+  "name": "Artist 102",
+  "image": "placeholder.png",
+  "created": 1757730341
+},
+{
+  "id": "100103",
+  "name": "Artist 103",
+  "image": "placeholder.png",
+  "created": 1757720341
+},
+{
+  "id": "100104",
+  "name": "Artist 104",
+  "image": "placeholder.png",
+  "created": 1757710341
+},
+{
+  "id": "100105",
+  "name": "Artist 105",
+  "image": "placeholder.png",
+  "created": 1757700341
+},
+{
+  "id": "100106",
+  "name": "Artist 106",
+  "image": "placeholder.png",
+  "created": 1757690341
+},
+{
+  "id": "100107",
+  "name": "Artist 107",
+  "image": "placeholder.png",
+  "created": 1757680341
+},
+{
+  "id": "100108",
+  "name": "Artist 108",
+  "image": "placeholder.png",
+  "created": 1757670341
+},
+{
+  "id": "100109",
+  "name": "Artist 109",
+  "image": "placeholder.png",
+  "created": 1757660341
+},
+{
+  "id": "100110",
+  "name": "Artist 110",
+  "image": "placeholder.png",
+  "created": 1757650341
+},
+{
+  "id": "100111",
+  "name": "Artist 111",
+  "image": "placeholder.png",
+  "created": 1757640341
+},
+{
+  "id": "100112",
+  "name": "Artist 112",
+  "image": "placeholder.png",
+  "created": 1757630341
+},
+{
+  "id": "100113",
+  "name": "Artist 113",
+  "image": "placeholder.png",
+  "created": 1757620341
+},
+{
+  "id": "100114",
+  "name": "Artist 114",
+  "image": "placeholder.png",
+  "created": 1757610341
+},
+{
+  "id": "100115",
+  "name": "Artist 115",
+  "image": "placeholder.png",
+  "created": 1757600341
+},
+{
+  "id": "100116",
+  "name": "Artist 116",
+  "image": "placeholder.png",
+  "created": 1757590341
+},
+{
+  "id": "100117",
+  "name": "Artist 117",
+  "image": "placeholder.png",
+  "created": 1757580341
+},
+{
+  "id": "100118",
+  "name": "Artist 118",
+  "image": "placeholder.png",
+  "created": 1757570341
+},
+{
+  "id": "100119",
+  "name": "Artist 119",
+  "image": "placeholder.png",
+  "created": 1757560341
+},
+{
+  "id": "100120",
+  "name": "Artist 120",
+  "image": "placeholder.png",
+  "created": 1757550341
+},
+{
+  "id": "100121",
+  "name": "Artist 121",
+  "image": "placeholder.png",
+  "created": 1757540341
+},
+{
+  "id": "100122",
+  "name": "Artist 122",
+  "image": "placeholder.png",
+  "created": 1757530341
+},
+{
+  "id": "100123",
+  "name": "Artist 123",
+  "image": "placeholder.png",
+  "created": 1757520341
+},
+{
+  "id": "100124",
+  "name": "Artist 124",
+  "image": "placeholder.png",
+  "created": 1757510341
+},
+{
+  "id": "100125",
+  "name": "Artist 125",
+  "image": "placeholder.png",
+  "created": 1757500341
+},
+{
+  "id": "100126",
+  "name": "Artist 126",
+  "image": "placeholder.png",
+  "created": 1757490341
+},
+{
+  "id": "100127",
+  "name": "Artist 127",
+  "image": "placeholder.png",
+  "created": 1757480341
+},
+{
+  "id": "100128",
+  "name": "Artist 128",
+  "image": "placeholder.png",
+  "created": 1757470341
+},
+{
+  "id": "100129",
+  "name": "Artist 129",
+  "image": "placeholder.png",
+  "created": 1757460341
+},
+{
+  "id": "100130",
+  "name": "Artist 130",
+  "image": "placeholder.png",
+  "created": 1757450341
+},
+{
+  "id": "100131",
+  "name": "Artist 131",
+  "image": "placeholder.png",
+  "created": 1757440341
+},
+{
+  "id": "100132",
+  "name": "Artist 132",
+  "image": "placeholder.png",
+  "created": 1757430341
+},
+{
+  "id": "100133",
+  "name": "Artist 133",
+  "image": "placeholder.png",
+  "created": 1757420341
+},
+{
+  "id": "100134",
+  "name": "Artist 134",
+  "image": "placeholder.png",
+  "created": 1757410341
+},
+{
+  "id": "100135",
+  "name": "Artist 135",
+  "image": "placeholder.png",
+  "created": 1757400341
+},
+{
+  "id": "100136",
+  "name": "Artist 136",
+  "image": "placeholder.png",
+  "created": 1757390341
+},
+{
+  "id": "100137",
+  "name": "Artist 137",
+  "image": "placeholder.png",
+  "created": 1757380341
+},
+{
+  "id": "100138",
+  "name": "Artist 138",
+  "image": "placeholder.png",
+  "created": 1757370341
+},
+{
+  "id": "100139",
+  "name": "Artist 139",
+  "image": "placeholder.png",
+  "created": 1757360341
+},
+{
+  "id": "100140",
+  "name": "Artist 140",
+  "image": "placeholder.png",
+  "created": 1757350341
+},
+{
+  "id": "100141",
+  "name": "Artist 141",
+  "image": "placeholder.png",
+  "created": 1757340341
+},
+{
+  "id": "100142",
+  "name": "Artist 142",
+  "image": "placeholder.png",
+  "created": 1757330341
+},
+{
+  "id": "100143",
+  "name": "Artist 143",
+  "image": "placeholder.png",
+  "created": 1757320341
+},
+{
+  "id": "100144",
+  "name": "Artist 144",
+  "image": "placeholder.png",
+  "created": 1757310341
+},
+{
+  "id": "100145",
+  "name": "Artist 145",
+  "image": "placeholder.png",
+  "created": 1757300341
+},
+{
+  "id": "100146",
+  "name": "Artist 146",
+  "image": "placeholder.png",
+  "created": 1757290341
+},
+{
+  "id": "100147",
+  "name": "Artist 147",
+  "image": "placeholder.png",
+  "created": 1757280341
+},
+{
+  "id": "100148",
+  "name": "Artist 148",
+  "image": "placeholder.png",
+  "created": 1757270341
+},
+{
+  "id": "100149",
+  "name": "Artist 149",
+  "image": "placeholder.png",
+  "created": 1757260341
+},
+{
+  "id": "100150",
+  "name": "Artist 150",
+  "image": "placeholder.png",
+  "created": 1757250341
+}
 ];
